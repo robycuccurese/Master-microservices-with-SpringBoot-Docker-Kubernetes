e55d6b56652ba721c5a5d629d63d67eb5f562315
Roberto Cuccurese <robycuccurese@gmail.com>
2026-02-01 18:52:28 +0100

    Initial commit

---
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -0,0 +1,24 @@
+package com.eazybytes.accounts.dto;
+
+import com.eazybytes.accounts.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class AccountsDto {
+    @NotEmpty(message = "Account number can not be null or empty")
+    @Pattern(regexp="(^$|[0-9]{10})", message = "Account number must be 10 digits")
+    private Long accountNumber;
+
+    @NotEmpty(message = "Account type can not be null or empty")
+    private String accountType;
+
+    @NotEmpty(message = "Branch address can not be null or empty")
+    private String branchAddress;
+}
---
+++ accounts/src/main/java/com/eazybytes/accounts/entity/Accounts.java
@@ -0,0 +1,19 @@
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+public class Accounts extends BaseEntity {
+    @Column(name="customer_id")
+    private Long customerId;
+
+    @Id
+    @Column(name="account_number")
+    private Long accountNumber;
+
+    private String accountType;
+
+    private String branchAddress;
+}
